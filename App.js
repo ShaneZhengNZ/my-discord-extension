ea6d4129e874d143874728eef034a54e655d6697
Shane <shane@myracelab.com>
2022-08-21 14:27:28 +1200

    Add native base

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NativeBaseProvider, Heading } from 'native-base';
 import { initializeApp } from 'firebase/app';
 import Constants from 'expo-constants';
 
@@ -21,10 +22,13 @@ initializeApp(firebaseConfig);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+        <Heading>Heading</Heading>
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
