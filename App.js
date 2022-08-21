8a94e57784ee97754eca44e78192fc3633c0f54c
Shane <shane@myracelab.com>
2022-08-21 14:01:07 +1200

    Get firebase working.

--- App.js
+++ App.js
@@ -1,5 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { initializeApp } from 'firebase/app';
+import Constants from 'expo-constants';
+
+// Initialize Firebase
+const firebaseConfig =
+  Constants.manifest && Constants.manifest.extra
+    ? {
+        apiKey: Constants.manifest.extra['firebaseApiKey'],
+        authDomain: Constants.manifest.extra['firebaseAuthDomain'],
+        projectId: Constants.manifest.extra['firebaseProjectId'],
+        storageBucket: Constants.manifest.extra['firebaseStorageBucket'],
+        messagingSenderId:
+          Constants.manifest.extra['firebaseMessagingSenderId'],
+        appId: Constants.manifest.extra['firebaseAppId'],
+      }
+    : {};
+
+initializeApp(firebaseConfig);
 
 export default function App() {
   return (
